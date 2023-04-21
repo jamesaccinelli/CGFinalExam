7ae97eaf92ce148a2e158dc490355de27ed177d7
jamesaccinelli <james.accinelli@ontariotechu.net>
2023-04-20 23:05:16 -0400

    setup

---
+++ CGFinal/Assets/Shaders/ColourGrade/ColourGrade.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourGrade : MonoBehaviour
+{
+    //public Shader awesomeShader = null;
+    public Material m_renderMaterial;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, m_renderMaterial);
+    }
+}
